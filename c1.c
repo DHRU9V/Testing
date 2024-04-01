06ede18900edd1efeec5942583def249ef9db8e7
DHRU9V <144544589+DHRU9V@users.noreply.github.com>
2024-04-01 18:12:13 +0530

    Add files via upload

---
+++ c1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int number1,number2,sum;
+    printf("\n Enter two elements: ");
+    scanf("%d",&number1);
+    scanf("%d",&number2);
+    sum=number1+number2;
+    printf("%d + %d = %d",number1,number2,sum);
+    return 0;
+}
